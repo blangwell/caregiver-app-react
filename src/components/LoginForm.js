eaa0e8b803ecf4ee369915f4b171f50ac6569f47
blangwell <b.langwell@outlook.com>
2021-05-10 22:35:26 -0700

    Connect LoginForm to backend via handleSubmit and test

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,20 +1,36 @@
 import { Formik, Form } from 'formik';
+import axios from 'axios';
 import TextInput from './TextInput';
 import * as Yup from 'yup';
+
+const handleSubmit = async (values) => {
+  console.log(values);
+  try {
+    const response = await axios.post('http://localhost:8000/login', {
+      email: values.email,
+      password: values.password,
+      withCredentials: true
+    });
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const LoginForm = () => {
   return (
     <Formik 
       initialValues={{ email: '', password: '' }}
       validationSchema={Yup.object({
-        email: Yup.string().email().required('Email required'),
-        password: Yup.string().required('Please enter a password'),
+        email: Yup.string().email('Invalid email address').required('Required'),
+        password: Yup.string().required('Required'),
       })}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
           setSubmitting(false);
         }, 400);
+        handleSubmit(values);
       }}
     >
       <Form noValidate>
@@ -23,7 +39,7 @@ const LoginForm = () => {
           name="email"
           type="email"
           placeholder="joeschmoe@aol.com"
-        />
+        /><br/>
         <TextInput 
           label="Password"
           name="password"
